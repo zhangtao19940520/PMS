eb69e5d9c1f254abd0eb2d2320dec7b5421f6aca
zhangtao <zhangtao01@ehousechina.com>
2019-11-21 17:40:17 +0800

    user_center（upload headers） -- 25%

--- PMS/settings.py
+++ PMS/settings.py
@@ -135,7 +135,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 SESSION_COOKIE_AGE = 60 * 60 * 24
